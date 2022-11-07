ac3fa24b070dec5db175ef191d68701fcc6e3333
Sajia09 <sajiaa09@gmail.com>
2022-11-08 01:50:07 +0600

    first commit

---
+++ src/Pages/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2 className="4xl">Services</h2>
+        </div>
+    );
+};
+
+export default Services;
---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2 className="4xl">Footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
