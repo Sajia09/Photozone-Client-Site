f5f4b21504460e3ae67ecbebb5bcef2690f871e5
Sajia09 <sajiaa09@gmail.com>
2022-11-08 17:57:44 +0600

    service added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,8 @@
+import { Button } from 'flowbite-react';
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import UseTitle from '../../Hooks/UseTitle'
+import ServiceCard from '../Services/ServiceCard';
 import Banner from './Banner/Banner';
 const Home = () => {
     UseTitle('Home');
@@ -8,7 +10,16 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h2 className="text-4xl">{services.length}</h2>
+            <h1 className="text-4xl text-blue-700 font-bold mt-20">Services Provided By PhotoZone</h1>
+            <p className="text-xl font-medium mt-4">PhotoZone Provide the best Photography Services!!Photography is the art, application, and practice of creating durable images by recording light, either electronically by means of an image sensor</p>
+            <div className='grid grid-cols-3 gap-4 my-5 mt-5'>
+            {
+                (services && services.slice(0,3).map(service =><ServiceCard key={service._id}
+                    service={service}
+                ></ServiceCard>))
+            }
+            </div>
+            <div><Link to='/services'><Button gradientDuoTone="cyanToBlue">See All</Button></Link></div>
         </div>
     );
 };
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const services = useLoaderData();
     return (
         <div>
-            <h2 className="4xl">Services</h2>
+            <h1 className="text-4xl text-blue-700 font-bold mt-5">Services Provided By PhotoZone</h1>
+            <p className="text-xl font-medium mt-4">PhotoZone Provide the best Photography Services!!Photography is the art, application, and practice of creating durable images by recording light, either electronically by means of an image sensor</p>
+            <div className='grid grid-cols-3 mt-10 gap-4'>
+            {
+                services.map(service =><ServiceCard 
+                    key={service._id}
+                    service={service}
+                ></ServiceCard>)
+            }
+        </div>
         </div>
     );
 };
