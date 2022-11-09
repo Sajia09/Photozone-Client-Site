032e982503fef94e7161a8fdd1fb0bfda3e909d4
Sajia09 <sajiaa09@gmail.com>
2022-11-09 16:09:50 +0600

    sign up

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import router from './Routes/Routes'
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className='max-w-screen-lg mx-auto'>
       <RouterProvider router={router}></RouterProvider>
+      <ToastContainer />
     </div>
   );
 }
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,49 +1,126 @@
-import React from 'react';
+import { Button, Card, Label, TextInput } from 'flowbite-react';
+import React, { useContext, useState } from 'react';
+import {  toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
+import { authContext } from '../../Contexts/AuthProvider/AuthProvider';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Signup = () => {
+
+    const { createUser, updateUserProfile } = useContext(authContext);
+    const [error, setError] = useState('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photoURL, email, password);
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setError('');
+                toast.success('Account created successfully!!')
+                form.reset();
+                handleUpdateUserProfile(name, photoURL);
+            })
+            .catch(e => {
+                console.error(e);
+                setError(e.message);
+            });
+    }
+
+    const handleUpdateUserProfile = (name, photoURL) => {
+        const profile = {
+            displayName: name,
+            photoURL: photoURL
+        }
+
+        updateUserProfile(profile)
+            .then(() => { })
+            .catch(error => console.error(error));
+    }
+
+
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-          Flowbite    
-      </a>
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Create and account
-              </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
-                  <div>
-                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                  </div>
-                  <div>
-                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div>
-                      <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div className="flex items-start">
-                      <div className="flex items-center h-5">
-                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                      </div>
-                      <div className="ml-3 text-sm">
-                        <label for="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                      </div>
-                  </div>
-                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <p className="font-medium text-primary-600 hover:underline dark:text-primary-500"><Link to='/login'>Login Here</Link></p>
-                  </p>
-              </form>
-          </div>
+        <div className='mx-20 p-20 bg-blue-100'>
+  <Card className='mx-20'>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+    <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="name"
+            value="Name"
+          />
+        </div>
+        <TextInput
+          id="name"
+          type="text"
+          placeholder="name"
+          name='name'
+          required={true}
+        />
       </div>
-  </div>
-</section>
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="email1"
+            value="Email"
+          />
+        </div>
+        <TextInput
+          id="email2"
+          type="email"
+          placeholder="email"
+          name='email'
+          required={true}
+        />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="photo"
+            value="PhotoURL"
+          />
+        </div>
+        <TextInput
+          id="email1"
+          type="text"
+          placeholder="PhotoURL"
+          name='photo'
+        />
+      </div>
+      
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="password1"
+            value="Your password"
+          />
+        </div>
+        <TextInput
+          id="password1"
+          type="password"
+          name='password'
+          required={true}
+          placeholder="password"
+          autoComplete='123456'
+        />
+      </div>
+      <p>Already have an account? <Link className='text-blue-500' to='/login'>Login Here</Link></p>
+      <Button type="submit">
+        Submit
+      </Button>
+    </form>
+    <Button type="submit" className='text-center'>
+        Sign In With Google
+      </Button>
+  </Card>
+</div>
     );
 };
 
